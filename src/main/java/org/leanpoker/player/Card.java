7d241709b73f128ebbae3901f1d032b8f4919af4
Tamás Marics <athalay@athalay-note.site>
2014-04-05 12:02:39 +0200

    2222

--- src/main/java/org/leanpoker/player/Card.java
+++ src/main/java/org/leanpoker/player/Card.java
@@ -4,10 +4,10 @@ package org.leanpoker.player;
  * Created by roland on 2014.04.05..
  */
 public class Card {
-    Integer rank;
+    String rank;
     String suit;
 
-    public Card(Integer rank, String suit) {
+    public Card(String rank, String suit) {
         this.rank = rank;
         this.suit = suit;
     }
